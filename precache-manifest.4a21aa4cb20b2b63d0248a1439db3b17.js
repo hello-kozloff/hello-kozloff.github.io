f005616b083b34e6afb962df469519878ca8b65e
Hello-Kozloff <hello.kozloff@gmail.com>
2019-10-29 14:52:31 +0300

    Instagram redesign Upload

---
+++ precache-manifest.4a21aa4cb20b2b63d0248a1439db3b17.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81b29255fae3f3d8d79",
+    "url": "/static/css/main.c3acc696.chunk.css"
+  },
+  {
+    "revision": "d81b29255fae3f3d8d79",
+    "url": "/static/js/main.45af4816.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8610c97185e85476c500",
+    "url": "/static/css/2.d67e5754.chunk.css"
+  },
+  {
+    "revision": "8610c97185e85476c500",
+    "url": "/static/js/2.a310c6a0.chunk.js"
+  },
+  {
+    "revision": "b6c5e3e80bd1ac4dd4d6d49d17418024",
+    "url": "/static/media/icon-trending.b6c5e3e8.svg"
+  },
+  {
+    "revision": "59b9284d8e7c26fc3ace479b9cc75ec2",
+    "url": "/static/media/logo.59b9284d.svg"
+  },
+  {
+    "revision": "bf309ea5d487cfff117e46bec11a40bd",
+    "url": "/static/media/icomoon.bf309ea5.eot"
+  },
+  {
+    "revision": "4418dfd9d6089690ec312812c6b462bb",
+    "url": "/static/media/icomoon.4418dfd9.ttf"
+  },
+  {
+    "revision": "78caeaa646567079b6560b9efe684f7b",
+    "url": "/static/media/icomoon.78caeaa6.woff"
+  },
+  {
+    "revision": "d4011c94050600fec23ce0efe4109a6b",
+    "url": "/static/media/icomoon.d4011c94.svg"
+  },
+  {
+    "revision": "1fbc15306ac130035f8b7945a5fffb52",
+    "url": "/index.html"
+  }
+];
